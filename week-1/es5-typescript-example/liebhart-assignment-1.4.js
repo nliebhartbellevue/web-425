aa8fddd3f2ba052add20e3d74b7b3a7cbed1cef8
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2019-12-20 14:14:44 -0600

    assignment-1.4

---
+++ week-1/es5-typescript-example/liebhart-assignment-1.4.js
@@ -0,0 +1,7 @@
+var empName = 'John Doe';
+var empPos = 'Sales Manager';
+// Employee of the Month function
+function empMonth(empName, empPos) {
+    return "Our New Employee of the Month is " + empName + " our " + empPos;
+}
+console.log(empMonth(empName, empPos));
