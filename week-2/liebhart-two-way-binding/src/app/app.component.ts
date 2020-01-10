ed45b2f1e6a3ab2149a12f564663593551773d96
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2020-01-09 19:55:28 -0600

    exercise 2.3

--- week-2/liebhart-two-way-binding/src/app/app.component.ts
+++ week-2/liebhart-two-way-binding/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  template: `
+    <br /><br /><br />
+    <div class="container-fluid two-way-form">
+      <h2>Two-Way Binding Example</h2>
+      <div class="form-group">
+        <input
+          class="form-control"
+          type="text"
+          placeholder="Enter your name..."
+          [(ngModel)]="name"
+        />
+
+        <button class="btn btn-primary form-control" (click)="name = ''">
+          Clear Name
+        </button>
+      </div>
+
+      <div class="form-group">
+        <p class="alert-success">Welcome to two-way binding, {{ name }}!</p>
+      </div>
+    </div>
+  `,
+  styles: [
+    `
+      .two-way-form {
+        width: 50%;
+      }
+    `
+  ]
 })
 export class AppComponent {
-  title = 'liebhart-two-way-binding';
+  name: string;
 }
