976747b119c95b12d0f7c11a4ea35320780fa413
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2020-01-09 19:26:56 -0600

    Exercise 2.2

--- week-2/liebhart-one-way-binding/src/app/app.component.ts
+++ week-2/liebhart-one-way-binding/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  template: `
+    <h1>{{ name }}</h1>
+    <button (click)="changeName()">Change Name</button>
+  `,
+  styles: [
+    `
+      h1 {
+        color: red;
+      }
+    `
+  ]
 })
 export class AppComponent {
-  title = 'liebhart-one-way-binding';
+  title = "liebhart-one-way-binding";
+
+  // class property
+  name: string = "Nathaniel Liebhart";
+
+  // change name function
+  changeName() {
+    this.name = "Nathaniel Dean Liebhart";
+  }
 }
