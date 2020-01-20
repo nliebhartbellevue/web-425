a1a608d95811fa43a56ef11f72356e37d5d5d615
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2020-01-20 14:16:02 -0600

    Exercise 3.3

---
+++ week-3/liebhart-child-routes/src/app/product-description/product-description.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-product-description",
+  template: `
+    <p>
+      This is a great product!
+    </p>
+  `,
+  styles: []
+})
+export class ProductDescriptionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
