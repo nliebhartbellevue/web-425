7d39fa4146297b2dac645d0104775903ed79294c
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2020-01-27 13:17:26 -0600

    Exercise 4.3

---
+++ week-4/liebhart-form-control/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { debounceTime } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  searchInput = new FormControl('');
+  convertedStockPrice: string;
+
+  constructor() {
+    this.searchInput.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe(stock => this.getStockQuoteFromServer(stock));
+  }
+
+  getStockQuoteFromServer(stock: string) {
+    this.convertedStockPrice = `The price of ${stock} is ${(100 * Math.random()).toFixed(4)}`;
+  }
+
+}
