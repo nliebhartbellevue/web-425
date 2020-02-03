9eb2c07bbf49ba5a2b704c26512454556b8bdcc3
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2020-02-03 15:50:46 -0600

    Assignment 5.4

---
+++ week-5/liebhart-md-in-action/src/app/app.component.ts
@@ -0,0 +1,16 @@
+/**
+ * Title: app.component.ts
+ * Author: Nathaniel Liebhart
+ * Date: February 3, 2020
+ * Description: Flex-Layout
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'liebhart-md-in-action';
+}
---
+++ week-5/liebhart-md-in-action/src/app/app.module.ts
@@ -0,0 +1,49 @@
+/**
+ * Title: app.module.ts
+ * Author: Nathaniel Liebhart
+ * Date: February 3, 2020
+ * Description: Flex-Layout
+ */
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      HomeComponent,
+      LoginComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MatCardModule,
+      MatButtonModule,
+      MatIconModule,
+      MatMenuModule,
+      MatToolbarModule,
+      MatFormFieldModule,
+      MatInputModule,
+      FlexLayoutModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
+
