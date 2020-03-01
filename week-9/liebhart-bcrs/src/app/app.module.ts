907d5a5dc15b9c055ec587e15498f471bbba034b
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2020-02-29 22:36:13 -0600

    appModule

--- week-9/liebhart-bcrs/src/app/app.module.ts
+++ week-9/liebhart-bcrs/src/app/app.module.ts
@@ -1,18 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+/**
+ * Title: app.module.ts
+ * Author: Nathaniel Liebhart
+ * Date: February 27, 2020
+ * Description: Bob's Computer Repair Shop
+ */
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+// material design component modules
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatInputModule } from "@angular/material/input";
+import { MatDialogModule } from "@angular/material";
+import { MatDividerModule } from "@angular/material/divider";
+// flex layout module
+import { FlexLayoutModule } from "@angular/flex-layout";
+// Components
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatDialogModule,
+    MatDividerModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
